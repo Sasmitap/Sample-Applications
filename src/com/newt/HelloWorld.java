7080ea379678b38ea450c7412d82d7e1d4db8a53
sasmitap <pradhansasmita1991@gmail.com>
2016-03-11 11:18:44 +0530

    commit by sasmita

---
+++ src/com/newt/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.newt;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hi Newt");
+		System.out.println("Hi Sasmita !!! Welcome to NEWT");
+	}
+
+}
